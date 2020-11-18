57b16338abda1f336ef17b53ed46cd623685fc44
JeutronicGoat <72464860+JeutronicGoat@users.noreply.github.com>
2020-11-18 18:47:44 -0500

    Update CreateEventActivity.java

--- app/src/main/java/com/cosc3506/ccms/CreateEventActivity.java
+++ app/src/main/java/com/cosc3506/ccms/CreateEventActivity.java
@@ -1,8 +1,10 @@
 package com.cosc3506.ccms;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class CreateEventActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class CreateEventActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
+
+        Button createNewEventBtn = (Button)findViewById(R.id.createNewEventButton);
+        createNewEventBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(findViewById(R.id.newEventNameEditText.isEmpty()));
+            }
+        });
     }
 }
