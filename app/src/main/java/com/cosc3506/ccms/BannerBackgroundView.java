5fc8e9e36ea2f2978da2437749dcec9dd2501560
georgeatr <61525971+georgeatr@users.noreply.github.com>
2020-11-18 18:34:26 -0500

    Fixed club activity access

---
+++ app/src/main/java/com/cosc3506/ccms/BannerBackgroundView.java
@@ -0,0 +1,19 @@
+package com.cosc3506.ccms;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.util.jar.Attributes;
+
+public class BannerBackgroundView extends View {
+
+    public BannerBackgroundView(Context context, AttributeSet attrs){
+        super(context,attrs);
+    }
+    @Override
+    protected void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+    }
+}
