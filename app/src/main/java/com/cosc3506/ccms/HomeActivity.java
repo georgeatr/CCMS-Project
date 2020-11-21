28ec348a7eb178bd7c0c412b30533d5c07a48bd2
georgeatr <61525971+georgeatr@users.noreply.github.com>
2020-11-20 23:19:42 -0500

    Added style changes to taskbar

--- app/src/main/java/com/cosc3506/ccms/HomeActivity.java
+++ app/src/main/java/com/cosc3506/ccms/HomeActivity.java
@@ -27,8 +27,6 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         user = (User) getIntent().getExtras().getSerializable("user");
 
