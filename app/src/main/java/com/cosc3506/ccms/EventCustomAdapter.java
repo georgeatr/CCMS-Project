eef1d8f4d7513aacf7ccd00f7f9e8ddac650ece6
ZabiLarry <inakizabla@hotmail.com>
2020-11-20 20:02:00 -0600

    Revert "Revert "merged with jorge""
    This reverts commit 1e29ef4cbfede75e324c0df2d89f488999d27eed.

--- app/src/main/java/com/cosc3506/ccms/EventCustomAdapter.java
+++ app/src/main/java/com/cosc3506/ccms/EventCustomAdapter.java
@@ -3,27 +3,21 @@ package com.cosc3506.ccms;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.lang.reflect.Array;
-import java.nio.channels.Selector;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder> {
+public class EventCustomAdapter extends RecyclerView.Adapter<EventCustomAdapter.ViewHolder> {
 
     private ArrayList localDataSet;
     Context context;
-    Intent nextActivity;
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         private final TextView textView;
@@ -32,8 +26,8 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
         public ViewHolder(View view) {
             super(view);
             // Define click listener for the ViewHolder's View
-            textView = (TextView) view.findViewById(R.id.club_name);
-            linearLayout = (LinearLayout) view.findViewById(R.id.rowLayout_linear);
+            textView = (TextView) view.findViewById(R.id.event_name);
+            linearLayout = (LinearLayout) view.findViewById(R.id.event_rowLayout_linear);
         }
 
         public TextView getTextView() {
@@ -43,10 +37,9 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
 
     }
 
-    public CustomAdapter(Context context,Intent intent,ArrayList dataSet) {
+    public EventCustomAdapter(Context context, ArrayList dataSet) {
         this.context = context;
         localDataSet = dataSet;
-        nextActivity = intent;
     }
 
     // Create new views (invoked by the layout manager)
@@ -54,7 +47,7 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
     public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
         // Create a new view, which defines the UI of the list item
         View view = LayoutInflater.from(viewGroup.getContext())
-                .inflate(R.layout.rowlayout, viewGroup, false);
+                .inflate(R.layout.eventrowlayout, viewGroup, false);
 
         return new ViewHolder(view);
     }
@@ -67,16 +60,8 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
         // contents of the view with that element
         viewHolder.getTextView().setText((String)localDataSet.get(position));
 
-        //set background for the layout
-        viewHolder.getLinearLayout().setBackgroundColor(getRandomColor());
+        //viewHolder.getLinearLayout().setBackgroundColor(getRandomColor());
 
-        viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // display a toast with person name on item click
-                context.startActivity(nextActivity);
-            }
-        });
     }
 
     // Return the size of your dataset (invoked by the layout manager)
