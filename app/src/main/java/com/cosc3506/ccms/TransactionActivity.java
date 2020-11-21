7c05606fcfc2a106ba5acc13046a3fb1d15d72db
JeutronicGoat <72464860+JeutronicGoat@users.noreply.github.com>
2020-11-20 23:10:23 -0500

    transaction

--- app/src/main/java/com/cosc3506/ccms/TransactionActivity.java
+++ app/src/main/java/com/cosc3506/ccms/TransactionActivity.java
@@ -1,15 +1,9 @@
 package com.cosc3506.ccms;
 
 import android.os.Bundle;
-
-import com.google.android.material.appbar.CollapsingToolbarLayout;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
 
 public class TransactionActivity extends AppCompatActivity {
 
@@ -17,18 +11,12 @@ public class TransactionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transaction);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
-        toolBarLayout.setTitle(getTitle());
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Switch sign = findViewById(R.id.signSwitch);
+
+
+        
+
+
     }
 }
