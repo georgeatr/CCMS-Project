1db5e009826747645ec5b11b5b1c9d1f8b04d8c6
JeutronicGoat <72464860+JeutronicGoat@users.noreply.github.com>
2020-11-20 21:42:08 -0500

    Don't Click Button

--- app/src/main/java/com/cosc3506/ccms/CreateEventActivity.java
+++ app/src/main/java/com/cosc3506/ccms/CreateEventActivity.java
@@ -72,7 +72,7 @@ public class CreateEventActivity extends AppCompatActivity {
         }
         else {
 
-            
+
 
         }
     }
--- app/src/main/java/com/cosc3506/ccms/LoginActivity.java
+++ app/src/main/java/com/cosc3506/ccms/LoginActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
