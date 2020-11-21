fd77ca723c5ba8ea92bf848d37d6910e750cf7aa
JeutronicGoat <72464860+JeutronicGoat@users.noreply.github.com>
2020-11-20 21:06:25 -0500

    .

--- app/src/main/java/com/cosc3506/ccms/CreateEventActivity.java
+++ app/src/main/java/com/cosc3506/ccms/CreateEventActivity.java
@@ -11,22 +11,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class CreateEventActivity extends AppCompatActivity {
 
-    EditText nEN = findViewById(R.id.newEventNameEditText);
-    EditText nEDesc = findViewById(R.id.newEventDescriptionEditText);
-    EditText nEID = findViewById(R.id.newEventIDEditText);
-    EditText nEC = findViewById(R.id.newEventCapacityEditText);
-    EditText nEST = findViewById(R.id.newEventStartTimeEditText);
-    EditText nEET = findViewById(R.id.newEventEndTimeEditText);
-    EditText nEL = findViewById(R.id.newEventLocationEditText);
-    EditText nEDate = findViewById(R.id.newEventDateEditText);
-    EditText nEB = findViewById(R.id.newEventBudgetEditTextDec);
+    EditText nEN;
+    EditText nEDesc;
+    EditText nEID;
+    EditText nEC;
+    EditText nEST;
+    EditText nEET;
+    EditText nEL;
+    EditText nEDate;
+    EditText nEB;
 
-    Button createEvent = findViewById(R.id.createNewEventButton);
+    Button createEvent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
+
+        nEN = findViewById(R.id.newEventNameEditText);
+        nEDesc = findViewById(R.id.newEventDescriptionEditText);
+        nEID = findViewById(R.id.newEventIDEditText);
+        nEC = findViewById(R.id.newEventCapacityEditText);
+        nEST = findViewById(R.id.newEventStartTimeEditText);
+        nEET = findViewById(R.id.newEventEndTimeEditText);
+        nEL = findViewById(R.id.newEventLocationEditText);
+        nEDate = findViewById(R.id.newEventDateEditText);
+        nEB = findViewById(R.id.newEventBudgetEditTextDec);
 
         String newEventName = nEN.getText().toString();
         String newEventDescription = nEDesc.getText().toString();
@@ -40,6 +50,15 @@ public class CreateEventActivity extends AppCompatActivity {
 
         TextView fieldsNotFilledError = findViewById(R.id.errorTextView);
         fieldsNotFilledError.setVisibility(View.INVISIBLE);
+
+
+        createEvent = findViewById(R.id.createNewEventButton);
+        createEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
 
 
 
