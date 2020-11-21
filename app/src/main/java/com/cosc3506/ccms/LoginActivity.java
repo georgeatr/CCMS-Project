0ee97a45df27710a1436727b9265fc0a5de50868
JeutronicGoat <72464860+JeutronicGoat@users.noreply.github.com>
2020-11-20 20:21:49 -0500

    Update LoginActivity.java

--- app/src/main/java/com/cosc3506/ccms/LoginActivity.java
+++ app/src/main/java/com/cosc3506/ccms/LoginActivity.java
@@ -3,9 +3,8 @@ package com.cosc3506.ccms;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,6 +25,48 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        Button createAccount = findViewById(R.id.createNewAccountButton);
+        final EditText newStudentNumber = findViewById(R.id.newStudentNumberEditText);
+        final EditText newName = findViewById(R.id.newNameEditText);
+        final EditText newEmail = findViewById(R.id.newEmailEditText);
+        final EditText newPhoneNumber = findViewById(R.id.newPhoneNumberEditText);
+        final EditText newPassword = findViewById(R.id.newPasswordEditText);
+        final Button register = findViewById(R.id.registerButton);
+        //Register Fields are invisible
+        newStudentNumber.setVisibility(View.INVISIBLE);
+        newName.setVisibility(View.INVISIBLE);
+        newEmail.setVisibility(View.INVISIBLE);
+        newPhoneNumber.setVisibility(View.INVISIBLE);
+        newPassword.setVisibility(View.INVISIBLE);
+        register.setVisibility(View.INVISIBLE);
+
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                newStudentNumber.setVisibility(View.VISIBLE);
+                newName.setVisibility(View.VISIBLE);
+                newEmail.setVisibility(View.VISIBLE);
+                newPhoneNumber.setVisibility(View.VISIBLE);
+                newPassword.setVisibility(View.VISIBLE);
+                register.setVisibility(View.VISIBLE);
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                User u = new User(  newStudentNumber.getText().toString(),
+                        newName.getText().toString(),
+                        newPhoneNumber.getText().toString(),
+                        newEmail.getText().toString(),
+                        newPassword.getText().toString());
+
+                //Do something with the new User
+            }
+        });
+        
     }
 
     public void createUser(View view){
