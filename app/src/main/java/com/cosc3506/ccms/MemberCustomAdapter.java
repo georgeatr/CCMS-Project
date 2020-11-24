9fdb92dccf467d6d301a349a15eeafd080107fe0
georgeatr <61525971+georgeatr@users.noreply.github.com>
2020-11-23 22:52:18 -0500

    events and members layout change

--- app/src/main/java/com/cosc3506/ccms/MemberCustomAdapter.java
+++ app/src/main/java/com/cosc3506/ccms/MemberCustomAdapter.java
@@ -5,10 +5,13 @@ import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -19,19 +22,28 @@ public class MemberCustomAdapter extends RecyclerView.Adapter<MemberCustomAdapte
     Context context;
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        private final TextView textView;
+        private final TextView nameTextView;
+        private final TextView emailTextView;
+        private final Button promoteButton;
+        private final Button removeButton;
         private final LinearLayout linearLayout;
 
         public ViewHolder(View view) {
             super(view);
             // Define click listener for the ViewHolder's View
-            textView = (TextView) view.findViewById(R.id.transaction_name);
-            linearLayout = (LinearLayout) view.findViewById(R.id.transaction_rowLayout_linear);
+            nameTextView = (TextView) view.findViewById(R.id.member_name);
+            emailTextView = (TextView) view.findViewById(R.id.member_email);
+            promoteButton = (Button) view.findViewById(R.id.promote_button);
+            removeButton = (Button) view.findViewById(R.id.remove_button);
+            linearLayout = (LinearLayout) view.findViewById(R.id.member_rowLayout_linear);
         }
 
-        public TextView getTextView() {
-            return textView;
+        public TextView getNameTextView() {
+            return nameTextView;
         }
+        public TextView getEmailTextView() { return emailTextView; }
+        public Button getPromoteButton() { return promoteButton; }
+        public Button getRemoveButton() { return removeButton; }
         public LinearLayout getLinearLayout() { return linearLayout; }
 
     }
@@ -57,8 +69,8 @@ public class MemberCustomAdapter extends RecyclerView.Adapter<MemberCustomAdapte
 
         // Get element from your dataset at this position and replace the
         // contents of the view with that element
-        viewHolder.getTextView().setText((String)localDataSet.get(position));
-
+        viewHolder.getNameTextView().setText((String)localDataSet.get(position));
+        viewHolder.getEmailTextView().setText((String)localDataSet.get(position) + "@algomau.ca");
         //viewHolder.getLinearLayout().setBackgroundColor(getRandomColor());
 
     }
