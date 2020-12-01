671559e41644cd07c2e16e68a7d70912f6a97c2d
georgeatr <61525971+georgeatr@users.noreply.github.com>
2020-12-01 00:02:24 -0500

    Tried Extra things

--- app/src/main/java/com/cosc3506/ccms/EventCustomAdapter.java
+++ app/src/main/java/com/cosc3506/ccms/EventCustomAdapter.java
@@ -1,7 +1,6 @@
 package com.cosc3506.ccms;
 
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,33 +10,46 @@ import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.cosc3506.ccms.data.model.Event;
+
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 import java.util.Random;
 
 public class EventCustomAdapter extends RecyclerView.Adapter<EventCustomAdapter.ViewHolder> {
 
-    private ArrayList localDataSet;
+    private ArrayList<Event> localDataSet;
     Context context;
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        private final TextView textView;
+        private final TextView eventName;
+        private final TextView eventAddress;
+        private final TextView eventStart;
+        private final TextView eventEnd;
         private final LinearLayout linearLayout;
 
         public ViewHolder(View view) {
             super(view);
             // Define click listener for the ViewHolder's View
-            textView = (TextView) view.findViewById(R.id.event_name);
+            eventName = (TextView) view.findViewById(R.id.event_name);
+            eventAddress = (TextView) view.findViewById(R.id.event_address);
+            eventStart = (TextView) view.findViewById(R.id.event_start);
+            eventEnd = (TextView) view.findViewById(R.id.event_end);
             linearLayout = (LinearLayout) view.findViewById(R.id.event_rowLayout_linear);
         }
 
-        public TextView getTextView() {
-            return textView;
+        public TextView getEventName() {
+            return eventName;
         }
+        public TextView getEventAddress() { return eventAddress; }
+        public TextView getEventEnd() { return eventEnd; }
+        public TextView getEventStart() { return eventStart; }
         public LinearLayout getLinearLayout() { return linearLayout; }
 
     }
 
-    public EventCustomAdapter(Context context, ArrayList dataSet) {
+    public EventCustomAdapter(Context context, ArrayList<Event> dataSet) {
         this.context = context;
         localDataSet = dataSet;
     }
@@ -58,7 +70,10 @@ public class EventCustomAdapter extends RecyclerView.Adapter<EventCustomAdapter.
 
         // Get element from your dataset at this position and replace the
         // contents of the view with that element
-        viewHolder.getTextView().setText((String)localDataSet.get(position));
+        viewHolder.getEventName().setText((String)localDataSet.get(position).getName());
+        viewHolder.getEventAddress().setText((String)localDataSet.get(position).getAddress());
+        viewHolder.getEventStart().setText((String)localDataSet.get(position).getStartDate());
+        viewHolder.getEventEnd().setText((String)localDataSet.get(position).getEndDate());
 
         //viewHolder.getLinearLayout().setBackgroundColor(getRandomColor());
 
